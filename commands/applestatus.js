d7806172f6fc3f0c14d8ab16f8055b8c0859358b
Nythepegasus <nythepegasus84@gmail.com>
2022-01-07 17:30:09 -0500

    Added applestatus command

---
+++ commands/applestatus.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const webshot = require("node-webshot");
+const fs = require("fs");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("applestatus")
+    .setDescription("Sends a screenshot of Apple services statuses."),
+    async execute(interaction) {
+        interaction.deferReply({ ephemeral: true });
+        webshot("https://www.apple.com/support/systemstatus/", "status.jpeg", { screenSize: { width: 1920, height: 1080 }, shotSize: { width: 992, height: 837 }, shotOffset: { left: 462, top: 96 } }, function () {
+            interaction.editReply({ files: ["status.jpeg"] });
+            setTimeout(() => {
+                try {
+                    fs.unlinkSync("status.jpeg");
+                } catch (error) {
+                    console.log(error);
+                }
+            }, 5000);
+        });
+    },
+};
