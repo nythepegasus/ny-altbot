15d2c4d01ce91193a02cdb98583346c788d9e669
Nythepegasus <nythepegasus84@gmail.com>
2022-01-19 20:00:34 -0500

    We don't need the client object here

--- events/userJoin.js
+++ events/userJoin.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: "guildMemberAdd",
-    async execute(member, client) {
+    async execute(member) {
         let guild = member.guild;
-        let role = guild.roles.cache.find(role => role.name === "unverified");
+        let role = guild.roles.cache.find(role => role.name === "Unverified");
         member.roles.add(role);
     }
 }
