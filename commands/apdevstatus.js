ba8b3450a3a30a45bfd394db506436da0a0c8396
Nythepegasus <nythepegasus84@gmail.com>
2022-01-28 09:48:04 -0500

    Adding timeouts and getting rid of dev ephemeral response

--- commands/apdevstatus.js
+++ commands/apdevstatus.js
@@ -3,11 +3,12 @@ const webshot = require("node-webshot");
 const fs = require("fs");
 
 module.exports = {
+    cooldown: 5 * 60 * 1000,
     data: new SlashCommandBuilder()
     .setName("apdevstatus")
     .setDescription("Sends a screenshot of Apple Developer services statuses."),
     async execute(interaction) {
-        interaction.deferReply({ ephemeral: true });
+        interaction.deferReply();
         webshot("https://developer.apple.com/system-status/", "devstatus.jpeg", { screenSize: { width: 1920, height: 1080 }, shotSize: { width: 992, height: 837 }, shotOffset: { left: 462, top: 96 } }, function () {
             interaction.editReply({ files: ["devstatus.jpeg"] });
             setTimeout(() => {
--- commands/applestatus.js
+++ commands/applestatus.js
@@ -3,11 +3,12 @@ const webshot = require("node-webshot");
 const fs = require("fs");
 
 module.exports = {
+    cooldown: 5 * 60 * 1000,
     data: new SlashCommandBuilder()
     .setName("applestatus")
     .setDescription("Sends a screenshot of Apple services statuses."),
     async execute(interaction) {
-        interaction.deferReply({ ephemeral: true });
+        interaction.deferReply();
         webshot("https://www.apple.com/support/systemstatus/", "apstatus.jpeg", { screenSize: { width: 1920, height: 1080 }, shotSize: { width: 992, height: 940 }, shotOffset: { left: 462, top: 96 } }, function () {
             interaction.editReply({ files: ["apstatus.jpeg"] });
             setTimeout(() => {
