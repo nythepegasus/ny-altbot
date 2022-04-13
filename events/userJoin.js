c06dede705e56790a56f3faa7132e15bd3697ca6
Nythepegasus <nythepegasus84@gmail.com>
2022-04-13 14:58:33 -0400

    Clean slate

--- events/userJoin.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "guildMemberAdd",
-    async execute(member) {
-        let guild = member.guild;
-        let role = guild.roles.cache.find(role => role.name === "Unverified");
-        member.roles.add(role);
-    }
-}
