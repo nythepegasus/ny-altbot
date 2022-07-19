2e98f6a0dd8de72c9fea33aeb2c3cbb8cf3b3ce9
Nythepegasus <nythepegasus84@gmail.com>
2022-07-19 13:55:08 -0400

    Removing Apple status commands for now

--- modules/utils.py
+++ modules/utils.py
@@ -13,31 +13,11 @@ class UtilCog(Cog, name="Utility"):
 
     @app_commands.command(name="applestatus", description="Get Apple's current system statuses.")
     async def apple_status(self, interaction: discord.Interaction):
-        await interaction.response.defer()
-        browser = await launch()
-        page = await browser.newPage()
-        await page.goto("https://www.apple.com/support/systemstatus/")
-        await page.setViewport({'width': 1920, 'height': 1080})
-        image = await page.screenshot()
-        image = Image.open(io.BytesIO(image))
-        image = image.crop((464, 95, 1454, 982))
-        image.save("screenshot.png")
-        await interaction.followup.send(file=discord.File("screenshot.png"))
-        os.remove("screenshot.png")
+        pass
 
     @app_commands.command(name="dev-applestatus", description="Get Apple's current dev system statuses.")
     async def dev_apple_status(self, interaction: discord.Interaction):
-        await interaction.response.defer()
-        browser = await launch()
-        page = await browser.newPage()
-        await page.goto("https://developer.apple.com/support/system-status/")
-        await page.setViewport({'width': 1920, 'height': 1080})
-        image = await page.screenshot()
-        image = Image.open(io.BytesIO(image))
-        # image = image.crop((464, 95, 1454, 982))
-        image.save("screenshot.png")
-        await interaction.followup.send(file=discord.File("screenshot.png"))
-        os.remove("screenshot.png")
+        pass
 
 async def setup(client: Bot):
     await client.add_cog(UtilCog(client))
