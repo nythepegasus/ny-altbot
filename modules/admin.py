ead434a5c8220cc7898930da62276e930443f552
Nythepegasus <nythepegasus84@gmail.com>
2022-07-20 19:18:15 -0400

    Allow an easy way to reload cogs on the fly (owner only)

---
+++ modules/admin.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext.commands import Bot, Cog, command
+
+
+class AdminCog(Cog, name="Admin"):
+    def __init__(self, client):
+        self.client = client
+        self.description = "This module is only for the developer"
+
+    async def cog_check(self, ctx):
+        return await self.client.is_owner(ctx.author)
+
+    @command(name="load_cog", hidden=True, help="Loads a cog.")
+    async def load_cog(self, ctx, *, cog: str):
+        await ctx.message.delete()
+        try:
+            await self.client.load_extension(cog)
+        except Exception as e:
+            await ctx.author.send(f"**`ERROR:`**\n {type(e).__name__} - {e}")
+        else:
+            await ctx.send(f"`{cog}` has been loaded!", delete_after=5)
+
+    @command(name="unload_cog", hidden=True, help="Unloads a cog.")
+    async def unload_mod(self, ctx, *, cog: str):
+        await ctx.message.delete()
+        if cog == "modules.admin":
+            return await ctx.send("Cowardly refusing to unload admin cog.", delete_after=5)
+        try:
+            await self.client.unload_extension(cog)
+            return await ctx.send(f"Unloaded cog: `{cog}`", delete_after=5)
+        except Exception as e:
+            await ctx.author.send(f"**`ERROR:`**\n {type(e).__name__} - {e}")
+
+    @command(name="reload_cog", hidden=True, help="Reloads` a cog.")
+    async def unload_cog(self, ctx, *, cog: str):
+        await ctx.message.delete()
+        try:
+            await self.client.unload_extension(cog)
+            await self.client.load_extension(cog)
+            return await ctx.send(f"`{cog}` has been reloaded.", delete_after=5)
+        except Exception as e:
+            await ctx.author.send(f"**`ERROR:`**\n {type(e).__name__} - {e}")
+
+async def setup(client: Bot):
+    await client.add_cog(AdminCog(client))
+
