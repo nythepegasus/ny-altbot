3bd6bb9399965f9d5eaaf131104ce39f939a3361
Nythepegasus <nythepegasus84@gmail.com>
2022-07-22 20:48:52 -0400

    [Music] Implement skip command

--- modules/music.py
+++ modules/music.py
@@ -22,6 +22,15 @@ class Music(Cog):
     async def on_wavelink_node_ready(self, node: wavelink.Node):
         print(f"Node: <{node.identifier}> is ready!")
 
+    @Cog.listener()
+    async def on_wavelink_track_end(self, vc, track, reason):
+        print(f"{track} - {reason}")
+        if not vc.queue.is_empty:
+            new = await vc.queue.get_wait()
+            await vc.play(new)
+        else:
+            await vc.stop()
+
     @app_commands.command(name="play", description="Play song through the bot.")
     async def play(self, interaction: discord.Interaction, query: str):
         if interaction.user.voice is None:
@@ -39,6 +48,19 @@ class Music(Cog):
         else:
             await vc.queue.put_wait(track)
             await interaction.response.send_message(f"Added `{track.title}` to the queue...", ephemeral=True)
+
+    @app_commands.command(name="skip", description="Skip song.")
+    async def skip(self, interaction: discord.Interaction):
+        if not interaction.guild.voice_client:
+            vc: wavelink.Player = await interaction.user.voice.channel.connect(cls=wavelink.Player)
+        else:
+            vc: wavelink.Player = interaction.guild.voice_client
+
+        if not vc.is_playing():
+            return await interaction.response.send_message("Not currently playing anything!", ephemeral=True)
+        else:
+            await vc.stop()
+            return await interaction.response.send_message(f"Skipped song, playing `{vc.queue[0] or vc.queue}`")
 
 async def setup(client: Bot):
     await client.add_cog(Music(client))
