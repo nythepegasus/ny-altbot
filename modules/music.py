e7ebf1dcb7090df74b5ae9944219e3d34e0d004a
Nythepegasus <nythepegasus84@gmail.com>
2022-07-23 22:43:08 -0400

    Added a rough beginning of queue command

--- modules/music.py
+++ modules/music.py
@@ -14,7 +14,7 @@ class Music(Cog):
         await self.client.wait_until_ready()
 
         await wavelink.NodePool.create_node(bot=self.client,
-                                            host="0.0.0.0",
+                                            host="45.33.29.114",
                                             port=2333,
                                             password="TwiNkie45?!")
 
@@ -48,6 +48,32 @@ class Music(Cog):
         else:
             await vc.queue.put_wait(track)
             await interaction.response.send_message(f"Added `{track.title}` to the queue...", ephemeral=True)
+
+    @app_commands.command(name="queue", description="See the current queue of songs.")
+    async def queue(self, interaction: discord.Interaction, page: int = 0):
+        """
+        Get queue, and slice each page of the queue
+        """
+        if not interaction.guild.voice_client:
+            vc: wavelink.Player = await interaction.user.voice.channel.connect(cls=wavelink.Player)
+        else:
+            vc: wavelink.Player = interaction.guild.voice_client
+
+        cur_queue = list(vc.queue._queue)
+        embed = discord.Embed(title="Current Queue")
+        embed.add_field(name="Currently Playing", value=vc.track.title, inline=True)
+        embed.add_field(name="Position in Queue", value=vc.queue.find_position(vc.track), inline=True)
+        embed.add_field(name="Length of Queue", value=vc.queue.count, inline=True)
+        duration = sum([song.length for song in cur_queue])
+        songs = ""
+        for p, song in enumerate(cur_queue[0:((page+1)*10)]):
+            duration += song.length
+            songs += f"`{p:02}.` ({song.title})[{song.info.uri}]"
+        embed.add_field(name="Duration of Queue", value=duration, inline=True)
+        embed.add_field(name="**Next Up**", value=songs, inline=False)
+        embed.set_footer(text=f"10/{len(cur_queue)}")
+
+        await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="skip", description="Skip song.")
     async def skip(self, interaction: discord.Interaction):
