e308b390c6e79d3ebb9473a75d67ed5bb84226e6
Nythepegasus <nythepegasus84@gmail.com>
2022-10-06 16:12:06 -0400

    Fix formatting

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,5 @@
 import asyncio
 import discord
-from discord import app_commands
 from discord.ext.commands import Bot, Cog
 
 
@@ -16,11 +15,11 @@ class UtilCog(Cog, name="Utility"):
         cyr = discord.utils.get(thread.guild.text_channels, name="choose-your-roles")
         await asyncio.sleep(1)
         await thread.starter_message.pin()
-        help_emb = discord.Embed(title="Help us help you!", description=f"We are going to try to help you " \
-        "to the best of our abilities, but before we can do so, make sure to give us information " \
-        "such as the devices you are using if you haven't taken the <#{cyr.id}> survey!\n\nAlso " \
-        "please have patience, I know having an issue can be very frustrating, but all helpers " \
-        "here are community volunteers, so please treat them respectfully!")
+        help_emb = discord.Embed(title="Help us help you!", description=f"We are going to try to help you "
+                 "to the best of our abilities, but before we can do so, make sure to give us information "
+                f"such as the devices you are using if you haven't taken the <#{cyr.id}> survey!\n\nAlso "
+                "please have patience, I know having an issue can be very frustrating, but all helpers "
+                "here are community volunteers, so please treat them respectfully!")
         await thread.starter_message.reply(embed=help_emb)
 
 
