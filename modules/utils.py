ec17407ae98abee284be3fda5baccfbcc522d3c3
Nythepegasus <nythepegasus84@gmail.com>
2022-10-06 16:00:46 -0400

    Remove these commands for now

--- modules/utils.py
+++ modules/utils.py
@@ -7,14 +7,6 @@ class UtilCog(Cog, name="Utility"):
     def __init__(self, client: Bot):
         self.client = client
 
-    @app_commands.command(name="applestatus", description="Get Apple's current system statuses.")
-    async def apple_status(self, interaction: discord.Interaction):
-        pass
-
-    @app_commands.command(name="dev-applestatus", description="Get Apple's current dev system statuses.")
-    async def dev_apple_status(self, interaction: discord.Interaction):
-        pass
-
 
 async def setup(client: Bot):
     await client.add_cog(UtilCog(client))
