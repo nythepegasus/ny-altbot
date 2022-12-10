9029559f4f3dac081bbebe8412443e4ebb795fc5
Nythepegasus <nythepegasus84@gmail.com>
2022-12-10 09:13:53 -0500

    Fix import

--- modules/admin.py
+++ modules/admin.py
@@ -1,6 +1,7 @@
 import json
 import asyncio
 import discord
+from discord.ext import commands
 from discord.ext.commands import Bot, Cog, command, ExtensionNotLoaded, ExtensionAlreadyLoaded, ExtensionNotFound
 
 
